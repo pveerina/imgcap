fixed scaling issues v2

tlstm/tlstm.py
@@ -19,12 +19,13 @@ def make_onehot(index, length):
 
 class TLSTM:
 
-    def __init__(self,wvecDim, middleDim, paramDim, numWords,mbSize=30,rho=1e-4, topLayer = None):
+    def __init__(self,wvecDim, middleDim, paramDim, numWords,mbSize=30, scale=1, rho=1e-4, topLayer = None):
         self.wvecDim = wvecDim
         self.middleDim = middleDim
         self.paramDim = paramDim
         self.numWords = numWords
         self.mbSize = mbSize
+        self.scale = scale
         self.defaultVec = lambda : np.zeros((wvecDim,))
         self.rho = rho
         self.topLayer = topLayer
@@ -248,9 +249,8 @@ class TLSTM:
             self.backProp(tree.root, cerror)
 
         # scale cost and grad by mb size
-        scale = (1./(self.mbSize**2))
         for v in self.dL.itervalues():
-            v *=scale
+            v *=self.scale
 
         # Add L2 Regularization
         cost += (self.rho/2)*np.sum(self.Wf**2)
@@ -312,13 +312,13 @@ class TLSTM:
                 for y in x:
                     if type(y) == list:
                         for z in x:
-                            z *= scale
+                            z *= self.scale
                     else:
-                        y *= scale
+                        y *= self.scale
             else:
-                x *= scale
+                x *= self.scale
 
-        return scale*cost
+        return self.scale*cost
 
     def forwardProp(self,node, correct=[], guess=[]):
         cost  =  total = 0.0

tlstm/twin.py
@@ -2,12 +2,13 @@ import numpy as np
 from collections import Counter
 
 class Twin:
-	def __init__(self, sentenceDim, imageDim, sharedDim, numLayers, reg=1e-4):
+	def __init__(self, sentenceDim, imageDim, sharedDim, numLayers, scale, reg=1e-4):
 		self.sentenceDim = sentenceDim
 		self.imageDim = imageDim
 		self.reg = reg
 		self.sharedDim = sharedDim
 		self.numLayers = numLayers
+		self.scale = scale
 		self.initParams()
 
 	def initParams(self):
@@ -63,11 +64,11 @@ class Twin:
 				x[:] = 0.
 
 	def costAndGrad(self, mbdata, test=False):
+		mbSize = len(mbdata)
 		cost = 0.0
 		batch_image_activations = []
 		batch_sentence_activations = []
 		self.clearGradients()
-		scale = 1./(len(mbdata)**2)
 
 		for i, (imageVec, sentenceVec) in enumerate(mbdata):
 			image_activations = self.forwardPropImage(imageVec)
@@ -100,7 +101,7 @@ class Twin:
 
 		# add in L2-regularization
 		cost += self.reg * .5 * (np.sum([np.sum(x**2) for x in self.sent_params]) + np.sum([np.sum(x**2) for x in self.img_params]))
-		cost *= scale
+		cost *= self.scale
 
 		img_input_grads = []
 		sentence_input_grads = []
@@ -114,10 +115,10 @@ class Twin:
 		for n in range(len(self.img_grads)):
 			self.img_grads[n] += self.reg * self.img_params[n]
 			self.sent_grads[n] += self.reg * self.sent_params[n]
-			self.img_grads[n] *= scale
-			self.img_biasGrads[n] *= scale
-			self.sent_grads[n] *= scale
-			self.seng_biasGrads[n] *= scale
+			self.img_grads[n] *= self.scale
+			self.img_biasGrads[n] *= self.scale
+			self.sent_grads[n] *= self.scale
+			self.seng_biasGrads[n] *= self.scale
 
 		return cost, sentence_input_grads
 