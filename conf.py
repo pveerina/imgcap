64870ffbd416cd9aa25a263e2bde644d3e1dda1b
Nick Dufour <ndufour@eunoia.stanford.edu>
2015-05-30 23:18:57 -0700

    additional commentary for the configuration file

--- conf.py
+++ conf.py
@@ -7,7 +7,13 @@ arguments to be parsed by the wrapper.
 # global parameters
 root = '/Users/ndufour/Dropbox/Class/CS224D/project/imgcap' # root directory
 alpha = 1e-2 # the learning rate
-optimizer = 'adagrad'
+# 1e-2 is the fastest the vanilla network can learn using SGD and no
+# regularization. it appears to be able to work with ADAGRAD as well,
+# but the long-term behavior hasn't been checked yet.
+#
+# the network also appears to learn fine using an alpha of 1e-2 even
+# when using the default regularization, which is a good sign.
+optimizer = 'sgd'
 
 # data handler parameters
 megabatch_size = 10000 # the size of the chunk to load into memory at a time
@@ -24,11 +30,11 @@ paramDim = 2 # the number of children to consider is given by
              # (2*paramDim), any children beyond this are not considered
 numWords = None # the size of the L matrix, defined by the runNet script
 mbSize = minibatch_size # the minibatch size
-rho = 0#1e-4 # the regularization constant
+rho = 1e-4 # the regularization constant
 
 # twin network parameters
 sentenceDim = middleDim # the dimensionality of the sentences
 imageDim = None # the dimensionality of the images; set by runNet
 sharedDim = 275 # the size of the final output
 numLayers = 2 # the number of layers to include, not including the first layer
-reg = 0#1e-4 # the regularization constant
+reg = 1e-4 # the regularization constant
